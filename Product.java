74b9769bb654dde0714a6939e59cba4fc2d7590a
lukasdewitz <61688125+lukasdewitz@users.noreply.github.com>
2020-04-14 15:43:52 +0200

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,19 @@
+public class Product
+{
+    private String name;
+    
+    public Product()
+    {
+        setName(name);
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    { 
+        this.name = name;
+    }
+}
